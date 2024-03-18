8fac24c7db88ecae92721a8796b2cca21f8fd111
Fang Ling <61139150+fang-ling@users.noreply.github.com>
2024-03-18 11:53:17 +0800

    Add red black tree implementation

---
+++ src/red_black_tree.h
@@ -0,0 +1,16 @@
+/*
+ * This source file is part of the C Collections open source project
+ *
+ * Copyright (c) 2024 Fang Ling
+ * Licensed under Apache License v2.0
+ *
+ * See https://github.com/fang-ling/C-Collections/blob/main/LICENSE for license
+ * information
+ */
+
+#ifndef red_black_tree_h
+#define red_black_tree_h
+
+
+
+#endif /* red_black_tree_h */
